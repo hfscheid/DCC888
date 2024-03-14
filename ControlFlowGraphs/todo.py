69c52ee22bae3875e4110dc2637b7b0af918a511
Henrique Furst <henrique.furst@softruck.com>
2024-03-14 06:29:44 -0300

    solutions for ControlFlowGraphs

--- ControlFlowGraphs/todo.py
+++ ControlFlowGraphs/todo.py
@@ -61,7 +61,35 @@ def test_min3(x, y, z):
         3
     """
     # TODO: Implement this method
+
+    # 0: lt x_lt_y x y
+    # 1: bt x_lt_y 2 5
+    # 2: lt x_lt_y_and_z x z
+    # 3: bt x_lt_y_and_z 4 8
+    # 4: add answer x 0
+    # 5: lt y_lt_z y z
+    # 6: bt y_lt_z 7 8
+    # 7: add answer y 0
+    # 8: add answer z 0
+
+    env = Env({"x": x, "y": y, "z": z, "zero": 0})
+    i0 = Lth("x_lt_y", "x", "y")
+    i2 = Lth("x_lt_y_and_z", "x", "z")
+    i4 = Add("answer", "x", "zero")
+    i5 = Lth("y_lt_z", "y", "z")
+    i7 = Add("answer", "y", "zero")
+    i8 = Add("answer", "z", "zero")
+    i1 = Bt("x_lt_y", i2, i5)
+    i3 = Bt("x_lt_y_and_z", i4, i8)
+    i6 = Bt("y_lt_z", i7, i8)
+
+    i0.add_next(i1)
+    i2.add_next(i3)
+    i5.add_next(i6)
+    interp(i0, env)
     return env.get("answer")
+
+
 
 
 def test_div(m, n):
@@ -77,6 +105,26 @@ def test_div(m, n):
         0
     """
     # TODO: Implement this method
+
+    # i0: lth inc m n
+    # i1: add answer inc
+    # i2: sub m m n
+    # i3: bt inc 0 4
+    # i4: add answer inc
+
+    env = Env({"m": m, "n": n, "minus": -1, "answer": 0})
+    i0 = Mul("minus_n", "n", "minus")
+    i1 = Lth("inc", "n", "m") 
+    i2 = Add("answer", "answer", "inc")
+    i3 = Add("m", "m", "minus_n")
+    i5 = Add("answer", "answer", "inc")
+    i4 = Bt("inc", i0, i5)
+    i0.add_next(i1)
+    i1.add_next(i2)
+    i2.add_next(i3)
+    i3.add_next(i4)
+    i4.add_next(i5)
+    interp(i0, env)
     return env.get("answer")
 
 
@@ -89,4 +137,20 @@ def test_fact(n):
         6
     """
     # TODO: Implement this method
+    # i0: mul answer answer n
+    # i1: add n n dec
+    # i2: lt repeat 1 n
+    # i3: bt repeat i0 i4
+    # i4: add n n dec
+
+    env = Env({"n": n, "dec": -1, "answer": 1, "one": 1})
+    i0 = Mul("answer", "answer", "n")
+    i1 = Add("n", "n", "dec")
+    i2 = Lth("repeat", "one", "n")
+    i4 = Add("n", "n", "dec")
+    i3 = Bt("repeat", i0, i4)
+    i0.add_next(i1)
+    i1.add_next(i2)
+    i2.add_next(i3)
+    interp(i0, env)
     return env.get("answer")
