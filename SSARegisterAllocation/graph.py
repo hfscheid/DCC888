9eeb5195c88da26cb9f6df659fdd66eacdf99ce2
Henrique Furst Scheid <hfscheid@gmail.com>
2024-07-21 19:26:57 -0300

    feat: full greedy register allocation with doctest

--- SSARegisterAllocation/graph.py
+++ SSARegisterAllocation/graph.py
@@ -59,54 +59,13 @@ class IntersectionGraph:
 def register_allocation(prog: list[Inst]):
     """
     TODO: add descricao de phi aqui
-
-        >>> env = Env({
-        ... 'result0': 0,
-        ... 'zero': 0, 'one': 1, 'minone': -1,
-        ... 'n0': 14, 'd': 5
-        ... })
-        >>> i0 = Lth('z', 'n0', 'd')
-        >>> i2 = Mul('negd', 'd', 'minone')
-        >>> i3 = Phi('result1', 'result0', 'result2')
-        >>> i4 = Phi('n1', 'n0', 'n2')
-        >>> i5 = Add('result2', 'result1', 'one')
-        >>> i6 = Add('n2', 'n1', 'negd')
-        >>> i7 = Geq('l', 'n2', 'd')
-        >>> i9 = Add('remainder', 'zero', 'n2')
-        >>> i10 = Add('end', 'zero', 'zero')
-        >>> i1 = Bt('z', i10)
-        >>> i8 = Bt('l', i3)
-        >>> prog = [i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10]
-        >>> for i in range(9):
-        ...     prog[i].add_next(prog[i+1])
+        >>> prog, env = new_euclid()
         >>> coloring, num_registers = register_allocation(prog)
-        >>> coloring
+        >>> num_registers
+        5
     """
     iGraph = IntersectionGraph(prog)
     return iGraph.greedy_coloring(iGraph.maximum_cardinality_search())
-
-
-def euclid() -> list[Inst]:
-    env = Env({
-        'result0': 0,
-        'zero': 0, 'one': 1, 'minone': -1,
-        'n0': 14, 'd': 5
-    })
-    i0 = Lth('z', 'n0', 'd')
-    i2 = Mul('negd', 'd', 'minone')
-    i3 = Phi('result1', 'result0', 'result2')
-    i4 = Phi('n1', 'n0', 'n2')
-    i5 = Add('result2', 'result1', 'one')
-    i6 = Add('n2', 'n1', 'negd')
-    i7 = Geq('l', 'n2', 'd')
-    i9 = Add('remainder', 'zero', 'n2')
-    i10 = Add('end', 'zero', 'zero')
-    i1 = Bt('z', i10)
-    i8 = Bt('l', i3)
-    prog = [i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10]
-    for i in range(9):
-        prog[i].add_next(prog[i+1])
-    return prog
 
 
 def new_euclid() -> list[Inst]:
